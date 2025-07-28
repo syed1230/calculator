9651073d2aa208d1d41e7a241cf5e92c7495b869
syed1230 <shaguftha1505@gmail.com>
2025-07-28 15:46:01 +0530

    Add files via upload

---
+++ arthametic d.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int a,b;
+int c;
+printf("enter a,b, values");
+scanf("%d%d",&a,&b);
+c=a+b;
+c=a+b;
+printf("\na+b=%d",c);
+c=a-b;
+printf("\na-b=%d",c);
+c=a*b;
+printf("\na*b=%d",c);
+c=a/b;
+printf("\na/b=%d",c);
+c=a%b;
+printf("\na%d=%d",c);
+}
