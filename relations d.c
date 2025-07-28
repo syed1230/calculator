9ceae7d6343e73d7548d612f5da76ddf832f7d87
syed1230 <shaguftha1505@gmail.com>
2025-07-28 15:59:19 +0530

    Add files via upload

---
+++ relations d.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a,b;
+printf("enter a,b values");
+scanf("%d%d",&a,&b);
+printf("\na<b=%d",a<b);
+printf("\na>b=%d",a>b);
+printf("\na>=%d",a>=b);
+printf("\na<=%d",a<=b);
+}
